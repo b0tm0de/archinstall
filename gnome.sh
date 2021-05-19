53ec2fa38b65627a4b8ea7f208b4e98dc92049be
Turgut Sezer <hackcity@gmail.com>
2021-05-19 22:20:38 +0300

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-sudo pacman -S --noconfirm xorg gdm gnome gnome-extra gnome-tweaks tracker arc-gtk-theme arc-icon-theme
+read -t 30 -r -s -p "Starting gnome install script. Enter to continue, ctrl + c to skip"
+
+sudo pacman -Syu --noconfirm xorg gdm gnome-tweaks tracker3 arc-gtk-theme arc-icon-theme gnome-shell-extensions gnome-control-center xdg-user-dirs gnome-keyring file-roller gedit gnome-calculator gnome-font-viewer gnome-logs gnome-menus gnome-screenshot gnome-settings-daemon gnome-system-monitor gnome-themes-extra nautilus sushi baobab
 
 sudo systemctl enable gdm
 
-echo "Gnome and xorg installation completed"
-read -t 10 -r -s -p "Warning! Rebooting, enter to continue or ctrl + c to abort...
-
-exit
-umount -a
-reboot
+# post_install.sh
+chmod +x post_install.sh
+sh post_install.sh
+# post_install.sh
