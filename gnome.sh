ec85a7e52aaad5ca8992f34f23eac0860098779d
Turgut Sezer <hackcity@gmail.com>
2021-05-20 21:30:22 +0300

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -4,7 +4,7 @@ set -e
 
 read -t 30 -r -s -p "Starting gnome install script. Enter to continue, ctrl + c to skip"
 
-sudo pacman -Syu --noconfirm gdm gnome-tweaks tracker3 arc-gtk-theme arc-icon-theme gnome-shell-extensions gnome-control-center xdg-user-dirs gnome-keyring file-roller gedit gnome-calculator gnome-font-viewer gnome-logs gnome-menus gnome-screenshot gnome-settings-daemon gnome-system-monitor gnome-themes-extra nautilus sushi baobab
+sudo pacman -Syu --noconfirm gdm gnome-tweaks tracker3 dconf-editor arc-gtk-theme arc-icon-theme gnome-shell-extensions gnome-control-center xdg-user-dirs gnome-keyring file-roller gedit gnome-calculator gnome-font-viewer gnome-logs gnome-menus gnome-screenshot gnome-settings-daemon gnome-system-monitor gnome-themes-extra nautilus sushi baobab
 
 systemctl enable gdm
 
