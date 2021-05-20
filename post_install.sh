a78ccbcc61831a7e3ca48c2fd140b6c3c23f6989
Turgut Sezer <hackcity@gmail.com>
2021-05-20 21:29:56 +0300

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -9,13 +9,21 @@ read -t 30 -r -s -p "Starting post install script. Enter to continue, ctrl + c t
 
 git clone https://aur.archlinux.org/yay.git
 cd yay
-makepkg -si --noconfirm
+makepkg -si --noconfirm PKGBUILD
 cd /
 
-snapper -c root create-config /
-mkdir /.snapshots
-chmod a+rx /.snapshots
-chown :b0tm0de /.snapshots
+sudo snapper -c root create-config /
+sudo btrfs subvolume delete /.snapshots
+sudo mkdir /.snapshots
+sudo chmod a+rx /.snapshots
+sudo chmod 750 /.snapshots
+sudo chown :b0tm0de /.snapshots
+sudo btrfs su cr /@snapshots
+mount -o noatime,space_cache=v2,subvol=@snapshots /dev/sda5 /.snapshots
+genfstab -U / >> /etc/fstab
+
+read -t 15 -r -s -p "add username b0tm0de in ALLOW_USERS"
+sudo nvim /etc/snapper/configs/root
 
 sudo systemctl enable --now snapper-timeline.timer
 sudo systemctl enable --now snapper-cleanup.timer
@@ -25,18 +33,18 @@ yay -S --noconfirm google-chrome chrome-gnome-shell snapper-gui-git snap-pac-gru
 
 sudo mkdir /etc/pacman.d/hooks
 sudo touch /etc/pacman.d/hooks/50-bootbackup.hook
-echo "[Trigger]"  >> /etc/pacman.d/hooks/50-bootbackup.hook
-echo "Operation = Upgrade" >> /etc/pacman.d/hooks/50-bootbackup.hook
-echo "Operation = Install" >> /etc/pacman.d/hooks/50-bootbackup.hook
-echo "Operation = Remove" >> /etc/pacman.d/hooks/50-bootbackup.hook
-echo "Type = Path" >> /etc/pacman.d/hooks/50-bootbackup.hook
-echo "Target = boot/*" >> /etc/pacman.d/hooks/50-bootbackup.hook
-echo "" >> /etc/pacman.d/hooks/50-bootbackup.hook
-echo "[Action]" >> /etc/pacman.d/hooks/50-bootbackup.hook
-echo "Depends = rsync" >> /etc/pacman.d/hooks/50-bootbackup.hook
-echo "Description = Backing up /boot..." >> /etc/pacman.d/hooks/50-bootbackup.hook
-echo "When = PreTransaction" >> /etc/pacman.d/hooks/50-bootbackup.hook
-echo "Exec = /usr/bin/rsync -a --delete /boot /.bootbackup" >> /etc/pacman.d/hooks/50-bootbackup.hook
+sudo echo "[Trigger]"  >> /etc/pacman.d/hooks/50-bootbackup.hook
+sudo echo "Operation = Upgrade" >> /etc/pacman.d/hooks/50-bootbackup.hook
+sudo echo "Operation = Install" >> /etc/pacman.d/hooks/50-bootbackup.hook
+sudo echo "Operation = Remove" >> /etc/pacman.d/hooks/50-bootbackup.hook
+sudo echo "Type = Path" >> /etc/pacman.d/hooks/50-bootbackup.hook
+sudo echo "Target = boot/*" >> /etc/pacman.d/hooks/50-bootbackup.hook
+sudo echo "" >> /etc/pacman.d/hooks/50-bootbackup.hook
+sudo echo "[Action]" >> /etc/pacman.d/hooks/50-bootbackup.hook
+sudo echo "Depends = rsync" >> /etc/pacman.d/hooks/50-bootbackup.hook
+sudo echo "Description = Backing up /boot..." >> /etc/pacman.d/hooks/50-bootbackup.hook
+sudo echo "When = PreTransaction" >> /etc/pacman.d/hooks/50-bootbackup.hook
+sudo echo "Exec = /usr/bin/rsync -a --delete /boot /.bootbackup" >> /etc/pacman.d/hooks/50-bootbackup.hook
 
 sudo rm -r ~/yay
 sudo rm -r /archinstall
