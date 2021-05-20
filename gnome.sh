581cdaa54f6a75efb7bbd8b6138e4a4b2f97918e
Turgut Sezer <hackcity@gmail.com>
2021-05-20 16:31:43 +0300

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -4,9 +4,9 @@ set -e
 
 read -t 30 -r -s -p "Starting gnome install script. Enter to continue, ctrl + c to skip"
 
-sudo pacman -Syu --noconfirm gnome-tweaks tracker3 arc-gtk-theme arc-icon-theme gnome-shell-extensions gnome-control-center xdg-user-dirs gnome-keyring file-roller gedit gnome-calculator gnome-font-viewer gnome-logs gnome-menus gnome-screenshot gnome-settings-daemon gnome-system-monitor gnome-themes-extra nautilus sushi baobab
+sudo pacman -Syu --noconfirm gdm gnome-tweaks tracker3 arc-gtk-theme arc-icon-theme gnome-shell-extensions gnome-control-center xdg-user-dirs gnome-keyring file-roller gedit gnome-calculator gnome-font-viewer gnome-logs gnome-menus gnome-screenshot gnome-settings-daemon gnome-system-monitor gnome-themes-extra nautilus sushi baobab
 
-sudo systemctl enable gdm
+systemctl enable gdm
 
 # post_install.sh
 chmod +x /archinstall/post_install.sh
