746f966799051b4f8369c02d73cfbadcdff16093
Turgut Sezer <hackcity@gmail.com>
2021-05-20 14:57:35 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -35,11 +35,16 @@ mount -o noatime,space_cache=v2,subvol=@home /dev/sda6 /mnt/home
 mount -o noatime,space_cache=v2,subvol=@var /dev/sda5 /mnt/var
 
 set +e
-pacstrap /mnt base base-devel linux linux-firmware intel-ucode neovim git reflector
-set -e
-genfstab -U /mnt >> /mnt/etc/fstab
-
 sleep 1
-read -t 5 -r -s -p "chaging root to /mnt enter to continue ctrl + c to break"
+
+pacstrap /mnt base base-devel linux linux-headers linux-firmware intel-ucode neovim git reflector
+sleep 1
+genfstab -U /mnt >> /mnt/etc/fstab
+cat /mnt/etc/fstab
+sleep 1
+read -t 10 -r -s -p "chaging root to /mnt enter to continue ctrl + c to break"
+sleep 1
+arch-chroot /mnt
+arch-chroot /mnt
 sleep 1
 arch-chroot /mnt
