8c31b493d3b191513e0e9eb7861d503999690619
Turgut Sezer <hackcity@gmail.com>
2021-05-21 21:44:31 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,30 +9,33 @@ reflector -c Turkey -a 24 --sort rate --save /etc/pacman.d/mirrorlist
 
 lsblk
 
-read -t 180 -r -s -p "### WARNING ### Formatting /dev/sda5 as BTRFS enter to continue, ctrl + c to break"
+read -t 180 -r -s -p "### WARNING ### Formatting /dev/sda4 as BTRFS enter to continue, ctrl + c to break"
+mkfs.btrfs -f /dev/sda4
+echo "Formatted /dev/sda4"
+
+read -t 60 -r -s -p "### WARNING ### Formatting /dev/sda5 as BTRFS enter to continue, ctrl + c to break"
 mkfs.btrfs -f /dev/sda5
 echo "Formatted /dev/sda5"
 
-read -t 60 -r -s -p "### WARNING ### Formatting /dev/sda6 as BTRFS enter to continue, ctrl + c to break"
-mkfs.btrfs -f /dev/sda6
-echo "Formatted /dev/sda6"
-
-mount /dev/sda5 /mnt
+mount /dev/sda4 /mnt
 mkdir mnthome
-mount /dev/sda6 /mnthome
+mount /dev/sda5 /mnthome
 btrfs su cr /mnt/@
 btrfs su cr /mnt/@snapshots
 btrfs su cr /mnt/@var
+btrfs su cr /mnt/@tmp
 btrfs su cr /mnthome/@home
 umount /mnthome
 umount /mnt
 rm -r /mnthome
 
-mount -o noatime,space_cache=v2,subvol=@ /dev/sda5 /mnt
-mkdir -p /mnt/{boot,home,var}
+mount -o noatime,space_cache=v2,subvol=@ /dev/sda4 /mnt
+mkdir -p /mnt/{boot,home,var,tmp}
 mount /dev/sda1 /mnt/boot
-mount -o noatime,space_cache=v2,subvol=@home /dev/sda6 /mnt/home
-mount -o noatime,space_cache=v2,subvol=@var /dev/sda5 /mnt/var
+mount -o noatime,space_cache=v2,subvol=@var /dev/sda4 /mnt/var
+mount -o noatime,space_cache=v2,subvol=@tmp /dev/sda4 /mnt/tmp
+mount -o noatime,space_cache=v2,subvol=@snapshots /dev/sda4 /.snapshots
+mount -o noatime,space_cache=v2,subvol=@home /dev/sda5 /mnt/home
 
 set +e
 sleep 1
