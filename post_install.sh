6dbab16110b01acb54536fe667d24a07de20bef6
Turgut Sezer <hackcity@gmail.com>
2021-05-22 19:20:38 +0300

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -12,14 +12,13 @@ cd ..
 sudo rm -r yay
 
 read -t 10 -r -s -p "installing: google-chrome chrome-gnome-shell"
-yay -S --noconfirm google-chrome chrome-gnome-shell 
-pacman -Sy --noconfirm timeshift grub-btrfs
+yay -S --noconfirm google-chrome chrome-gnome-shell timeshift
 
 sudo chmod a+rx /.snapshots
 sudo chmod 750 /.snapshots
 sudo chown :b0tm0de /.snapshots
 
-read -t 10 -r -s -p "installation completed, clearing scripts..."
+read -t 10 -r -s -p "installation completed, clearing scripts... reboot suggested"
 
 sudo rm -r archinstall
-sudo reboot
+
