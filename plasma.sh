df2f3daa3c64e2d2cfb031c72a25f44128bae003
Turgut Sezer <hackcity@gmail.com>
2021-05-25 08:39:57 +0300

    Update plasma.sh

--- plasma.sh
+++ plasma.sh
@@ -4,5 +4,8 @@ set -e
 
 read -t 30 -r -s -p "Starting plasma install script. Enter to continue, ctrl + c to skip"
 
-pacman -Syu plasma-desktop plasma-nm plasma-pa dolphin konsole kdialog kinfocenter plasma-firewall 
+pacman -Syu plasma-desktop plasma-nm plasma-pa dolphin konsole kdialog kinfocenter plasma-firewall
+systemtcl enable sddm
+read -t 30 -r -s -p "installation done, restart recommended"
+
 #discover
