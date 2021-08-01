35ef01779fa81c06556aaba8e7ce638475da40f4
Turgut Sezer <hackcity@gmail.com>
2021-08-01 14:38:33 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,9 +16,9 @@ read -t 180 -r -s -p "### WARNING ### Formatting /dev/sda4 as BTRFS enter to con
 mkfs.btrfs -f /dev/sda4
 echo "Formatted /dev/sda4"
 
-#read -t 60 -r -s -p "### WARNING ### Formatting /dev/sda5 as BTRFS enter to continue, ctrl + c to break"
-#mkfs.btrfs -f /dev/sda5
-#echo "Formatted /dev/sda5"
+read -t 60 -r -s -p "### WARNING ### Formatting /dev/sda5 as BTRFS enter to continue, ctrl + c to break"
+mkfs.btrfs -f /dev/sda5
+echo "Formatted /dev/sda5"
 
 mount /dev/sda4 /mnt
 mkdir mnthome
@@ -26,7 +26,7 @@ mount /dev/sda5 /mnthome
 btrfs su cr /mnt/@
 btrfs su cr /mnt/@var
 btrfs su cr /mnt/@tmp
-#btrfs su cr /mnthome/@home
+btrfs su cr /mnthome/@home
 umount /mnthome
 umount /mnt
 rm -r /mnthome
@@ -42,7 +42,7 @@ mount -o noatime,space_cache=v2,subvol=@home /dev/sda5 /mnt/home
 set +e
 sleep 0.5
 
-pacstrap /mnt base base-devel linux-zen linux-zen-headers linux-firmware intel-ucode neovim git wget reflector
+pacstrap /mnt base base-devel linux linux-headers linux-firmware intel-ucode neovim git wget reflector
 sleep 0.5
 genfstab -U /mnt >> /mnt/etc/fstab
 sleep 0.5
