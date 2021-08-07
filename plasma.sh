aafa799024667610d9332904e214e1d14e2fa001
Turgut Sezer <hackcity@gmail.com>
2021-08-07 20:53:27 +0300

    Update plasma.sh

--- plasma.sh
+++ plasma.sh
@@ -4,8 +4,7 @@ set -e
 
 read -t 30 -r -s -p "Starting plasma install script. Enter to continue, ctrl + c to skip"
 
-pacman -Syu plasma-wayland-session plasma-desktop plasma-nm plasma-pa dolphin konsole kdialog kinfocenter plasma-firewall cronie kcron kate ffmpegthumbs svgpart okular spectacle kdegraphics-thumbnailers kcolorchooser gwenview ksystemlog kdeconnect ark kcalc kfind yakuake plasma-systemmonitor sddm-kcm plasma-browser-integration filelight partitionmanager kde-gtk-config kscreen kwrited powerdevil breeze-gtk rsync packagekit-qt5 plasma-disks numlockx
+pacman -Syu discover plasma-wayland-session plasma-desktop plasma-nm plasma-pa dolphin konsole kdialog kinfocenter plasma-firewall cronie kcron kate ffmpegthumbs svgpart okular spectacle kdegraphics-thumbnailers kcolorchooser gwenview ksystemlog kdeconnect ark kcalc kfind yakuake plasma-systemmonitor sddm-kcm plasma-browser-integration filelight partitionmanager kde-gtk-config kscreen kwrited powerdevil breeze-gtk rsync packagekit-qt5 plasma-disks numlockx
 systemtcl enable sddm
 read -t 30 -r -s -p "installation done, restart recommended"
 
-#discover
